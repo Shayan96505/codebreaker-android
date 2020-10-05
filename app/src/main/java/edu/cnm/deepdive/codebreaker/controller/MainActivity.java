acd2b8b39da620d12c7cc3825e455516bf0afe34
Shayan <sgolafshani@cnm.edu>
2020-10-05 17:38:03 -0600

    Fixed restart method in codebreaker and made sure to re-import codebreaker.jar. Up-to-date

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -3,8 +3,6 @@ package edu.cnm.deepdive.codebreaker.controller;
 import android.graphics.Color;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -16,17 +14,17 @@ import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.adapter.GuessAdapter;
 import edu.cnm.deepdive.codebreaker.model.Code.Guess;
 import edu.cnm.deepdive.codebreaker.model.Game;
+import edu.cnm.deepdive.codebreaker.viewmodel.MainViewModel;
 import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener{
-private static final String POOL = "ROYGBIV";
-private static final int CODE_LENGTH = 4;
-  private static final  int[] colorValues =
+public class MainActivity extends AppCompatActivity {
+
+  private static final int[] colorValues =
       {Color.RED, 0xffffa500, Color.YELLOW, Color.GREEN, Color.BLUE, 0xff4b0082, 0xffee82ee};
   private static final Map<Character, Integer> colorMap =
-      buildColorMap(POOL.toCharArray(), colorValues);
+      buildColorMap(MainViewModel.POOL.toCharArray(), colorValues);
 
   private ListView guessList;
   private EditText guess;
@@ -42,7 +40,7 @@ private static final int CODE_LENGTH = 4;
     guessList = findViewById(R.id.guess_list);
     guess = findViewById(R.id.guess);
     submit = findViewById(R.id.submit);
-    submit.setOnClickListener(this);
+    submit.setOnClickListener((v) -> recordGuess());
     adapter = new GuessAdapter(this, colorMap);
     rng = new SecureRandom();
     startGame();
@@ -53,13 +51,13 @@ private static final int CODE_LENGTH = 4;
   public boolean onCreateOptionsMenu(Menu menu) {
     super.onCreateOptionsMenu(menu);
     getMenuInflater().inflate(R.menu.options, menu);
-    return  true;
+    return true;
   }
 
   @Override
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     boolean handled = true;
-    switch(item.getItemId()){
+    switch (item.getItemId()) {
       case R.id.new_game:
         startGame();
         break;
@@ -72,13 +70,12 @@ private static final int CODE_LENGTH = 4;
     return handled;
   }
 
-  @Override
-  public void onClick(View v) {
+  private void recordGuess() {
     try {
       String text = this.guess.getText().toString().toUpperCase();
       Guess guess = game.guess(text);
       adapter.add(guess);
-      guessList.setSelection(adapter.getCount()-1);
+      guessList.setSelection(adapter.getCount() - 1);
       this.guess.setText("");
       game.guess(text);
 
@@ -86,8 +83,9 @@ private static final int CODE_LENGTH = 4;
       Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
     }
   }
+
   private void startGame() {
-    game = new Game(POOL,CODE_LENGTH, rng);
+    game = new Game(MainViewModel.POOL, MainViewModel.CODE_LENGTH, rng);
     resetList();
   }
 
@@ -101,7 +99,7 @@ private static final int CODE_LENGTH = 4;
     guessList.setAdapter(adapter);
   }
 
-  private static Map<Character, Integer> buildColorMap(char[] chars, int [] values){
+  private static Map<Character, Integer> buildColorMap(char[] chars, int[] values) {
     Map<Character, Integer> colorMap = new HashMap<>();
     for (int i = 0; i < chars.length; i++) {
       colorMap.put(chars[i], values[i]);
