c7a22980346d106967efd78737c108b6cc426191
Shayan <sgolafshani@cnm.edu>
2020-10-14 11:54:36 -0600

    Database implemented with basic functionality

--- app/src/main/java/edu/cnm/deepdive/codebreaker/CodebreakerApplication.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/CodebreakerApplication.java
@@ -1,7 +1,9 @@
 package edu.cnm.deepdive.codebreaker;
 
 import android.app.Application;
-import android.util.Log;
+import com.facebook.stetho.Stetho;
+import edu.cnm.deepdive.codebreaker.service.CodebreakerDatabase;
+import io.reactivex.schedulers.Schedulers;
 
 public class CodebreakerApplication extends Application {
 
@@ -9,6 +11,10 @@ public class CodebreakerApplication extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
-    Log.d(getClass().getName(), "Testing logging");
+    Stetho.initializeWithDefaults(this);
+    CodebreakerDatabase.setContext(this);
+    CodebreakerDatabase.getInstance().getGameDao().delete()
+        .subscribeOn(Schedulers.io())
+        .subscribe();
   }
 }
